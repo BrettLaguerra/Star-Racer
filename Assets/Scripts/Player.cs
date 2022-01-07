7af95164ab8670dffbdb16af30ee4899b2cb9046
Brett Laguerra <b.laguerra@yahoo.com>
2022-01-07 13:37:39 -0500

    Mirror Setup

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class Player : NetworkBehaviour
+{
+    void HandleMovement()
+    {
+        if (isLocalPlayer)
+        {
+            float moveHorizontal = Input.GetAxisRaw("Horizontal");
+            float moveVertical = Input.GetAxisRaw("Vertical");
+            Vector3 movement = new Vector3(moveHorizontal * .2f, moveVertical * .2f, 0);
+            transform.position = transform.position + movement;
+
+        }
+    }
+
+    void FixedUpdate()
+    {
+        HandleMovement();
+    }
+}
